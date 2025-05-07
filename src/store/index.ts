210c1f2b30ba0ff8f47d38d66957d476be8e9f6a
RickyBv1 <rickycortes668668@gmail.com>
2025-05-07 15:26:56 -0400

    feat: add sonner for toast notifications and implement sync middleware for user deletion
    - Added `sonner` package for toast notifications.
    - Integrated `Toaster` component in `App.tsx`.
    - Created `syncWithDatabaseMiddleware` to handle user deletion and show success/error notifications.
    - Updated `persistanceLocalStorageMiddleware` to use TypeScript types.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,51 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, type Middleware } from "@reduxjs/toolkit";
 import usersReducer from "./users/slice";
+import { toast } from "sonner";
 
-const persistanceLocalStorageMiddleware = (store) => (next) => (action) => {
-  next(action);
-  localStorage.setItem("__redux__state__", JSON.stringify(store.getState()));
-};
+const persistanceLocalStorageMiddleware: Middleware =
+  (store) => (next) => (action) => {
+    next(action);
+    localStorage.setItem("__redux__state__", JSON.stringify(store.getState()));
+  };
+
+const syncWithDatabaseMiddleware: Middleware =
+  (store) => (next) => (action) => {
+    const { type, payload } = action;
+    // const previousState = store.getState() as RootState;
+    next(action);
+
+    if (type === "users/deleteUserById") {
+      // const userIdToRemove = payload;
+      // const userToRemove = previousState.users.find(
+      //   (user) => user.id === userIdToRemove
+      // );
+
+      fetch(`https://jsonplaceholder.typicode.com/users/${payload}`, {
+        method: "DELETE",
+      })
+        .then((res) => {
+          if (res.ok) {
+            toast.success(`Usuario ${payload} eliminado correctamente`);
+          }
+          // throw new Error("Error al eliminar el usuario");
+        })
+        .catch((err) => {
+          // toast.error(`Error deleting user ${userIdToRemove}`);
+          // if (userToRemove) store.dispatch(rollbackUser(userToRemove));
+          console.log(err);
+          console.log("error");
+        });
+    }
+  };
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(persistanceLocalStorageMiddleware),
+    getDefaultMiddleware()
+      .concat(persistanceLocalStorageMiddleware)
+      .concat(syncWithDatabaseMiddleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
